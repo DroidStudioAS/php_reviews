efb7ea44b2a0b94501b5cdd5a9b526af6e0e02cf
DroidStudioAS <as.droiddevteam@gmail.com>
2023-11-20 08:11:50 +0100

    mock about page completed (responsive to all devices )

--- feedback/about.php
+++ feedback/about.php
@@ -15,17 +15,98 @@ include("inc/footer.php");
 <div class="main_container">
     <div class="first_row">
         <div class="headshot_container">
-            
+            <img src="/Aleksandar_Smiljanic/feedback/inc/logo.png"/>
         </div>
         <div class="bio_container">
-
+            <h3>Aleksandar Smiljanic Software </h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam eaque a nulla suscipit facere magni reprehenderit amet, qui ab unde, provident veritatis tempore dolorem soluta optio culpa molestias quas beatae.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati illo animi soluta quos officia cupiditate architecto, itaque facilis exercitationem, recusandae voluptatibus quaerat libero autem consequatur doloremque, doloribus maxime minima nisi!</p>
+            <p></p>
+            
         </div>
     </div>
     <div class="second_row">
-            <div class="skills_container">           
+            <div class="skills_container">   
+                <h4>Skills include but not limited to:</h4>   
+                <ul>
+                    <li>Java</li>
+                    <li>Android Studio</li>
+                    <li>HTML CSS and JS</li>
+                    <li>Vue.JS</li>
+                    <li>Node.JS</li>
+                    <li>PHP</li>
+                    <li>WordPress</li>
+                    <li>MySQL</li>
+                    <li>SQLite</li>
+                    <li>Git</li>
+                    <li>C# Fundamentals</li>
+                    <li>Unity 3D</li>
+                </ul>
             </div>
             <div class="projects_container">
-                
+                <h4>My Projects</h4>
+                <dl>
+                    <dt>
+                        Infinite Swipe
+                    </dt>
+                    <dd>
+                       2d arcade quiz game made with Vue.JS. Implemented GSAP for all front end animations
+                    </dd>
+                    <dt>
+                        FitCalc
+                    </dt>
+                    <dd>
+                      Android app built with Java in Android Studio. Takes measurments from the user to calculate
+                      users ideal ideal weight and daily calorie intake
+                    </dd>
+                    <dt>
+                        Running Tracker
+                    </dt>
+                    <dd>
+                    Android app built with Java in Android Studio. Uses location services to follow and show
+                    the users location. Built on top of the Google maps api.
+                    </dd>
+                    <dt>
+                        Flappy Bird Clone
+                    </dt>
+                    <dd>
+                      Unity 3D game made with c#. A complete working copy of the popular arcade game flappy bird 
+                    </dd>
+                    <dt>
+                        Calculator
+                    </dt>
+                    <dd>
+                    Android app built with Java in Android Studio. Works on all the 4 basic arythmetic operations with some
+                    "scientific" options
+                    </dd>
+                    <dt>
+                        QuizMe
+                    </dt>
+                    <dd>
+                    Android game built with Java in Android Studio. Uses the Trivia REST api to generate questions that
+                    the user is scored on.
+                    </dd>
+                    <dt>
+                        FlyingDonut
+                    </dt>
+                    <dd>
+                    Android game built with Java in Android Studio. Prompts the user to press on the character as fast as he can 
+                    to move it from the bottom to the top of the screen.
+                    </dd>
+                    <dt>
+                        ToDo
+                    </dt>
+                    <dd>
+                    Android app built with Java in Android Studio. Utilizes SQLite so that it stores all the users
+                    tasks offline.
+                    <dt>
+                        QuoteStop
+                    </dt>
+                    <dd>
+                    Android app built with Java in Android Studio. Uses a quote generating api, to deliver inspiring and motivating
+                    quotes to the user on demand. Also uses SQLIte to store the users favorite quotes offline.
+                    </dd>
+                </dl>
             </div>
     </div>
 
@@ -34,14 +115,14 @@ include("inc/footer.php");
 </body>
 </html>
 <style>
+   
     .main_container{
         width: 80vw;
-        height: 60vh;
+        height: 100vh;
         margin: 8px;
         display: flex;
         flex-flow: column nowrap;
 
-        background-color: pink;
 
         position: absolute;
         top: 36%;
@@ -58,29 +139,56 @@ include("inc/footer.php");
     }
 
     .headshot_container{
-        background-color: aqua;
+        width: 16vw;
+        height: 30vh;
+       
+    }
+    .headshot_container img{
         width: 16vw;
         height: 30vh;
     }
     .bio_container{
-        background-color: purple;
         width: 64vw;
         height: 30vh;
+        
+        display: flex;
+        flex-flow: column nowrap;
+        align-items:start;
+        justify-content: space-around;
+
+        padding-left: 2%;
+       
     }
     .skills_container{
-        background-color: rosybrown;
         width: 26.66vw;
-        height: 30vh;
+        height: 50vh;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        flex-flow: column nowrap;
     }
     .projects_container{
-        background-color: gold;
         width: 53.34vw;
-        height: 30vh;
+        height: 70vh;
+
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: center;
+    }
+    .projects_container dt{
+        font-weight: bold;
+    }
+    .projects_container dd{
+        font-style: italic;
     }
 
     @media(max-width:600px) {
         .main_container{
             top: 40%;
+            height: 100vh;
+        }
+        .headshot_container{
+          margin-bottom: 7%;
         }
         .first_row{
             flex-flow: column nowrap;
